208fdcf135a0fb38ead99c8bf85ccebaa5a24154
Andrey Arkhipov <arhipov11@yandex.ru>
2022-09-03 20:14:52 +0300

    added Circle components

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,23 +1,25 @@
-import React, {useState} from "react";
+import React, {FormEvent, useState} from "react";
 import {Button} from "../ui/button/button";
+import {Circle} from "../ui/circle/circle";
 import {Input} from "../ui/input/input";
 import {SolutionLayout} from "../ui/solution-layout/solution-layout";
 import "./string.css";
 
 export const StringComponent: React.FC = () => {
   const [string, setString] = useState("");
+  const [isShow, setIsShow] = useState(false);
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setIsShow(true);
+  }
   return (
     <SolutionLayout title="Строка">
-      <form
-        className="form"
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
-      >
-        <Input maxLength={11} isLimitText />
+      <form className="form" onSubmit={handleSubmit}>
+        <Input maxLength={11} isLimitText onChange={(e) => setString(e.currentTarget.value)} />
         <Button type="submit" extraClass="form__button" text="Развернуть" linkedList="small" />
-        <p>{string}</p>
       </form>
+      {isShow && string.split("").map((el, index) => <Circle letter={el} key={index} />)}
+      <Circle letter="A" head="Head" tail="tail" isSmall />
     </SolutionLayout>
   );
 };
