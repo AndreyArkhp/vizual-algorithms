979aaea9e2de590584cabfbb42c4c898fa8abf53
Andrey Arkhipov <arhipov11@yandex.ru>
2022-09-03 16:03:14 +0300

    Added form into StringComponent

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,23 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import React, {useState} from "react";
+import {Button} from "../ui/button/button";
+import {Input} from "../ui/input/input";
+import {SolutionLayout} from "../ui/solution-layout/solution-layout";
+import "./string.css";
 
 export const StringComponent: React.FC = () => {
+  const [string, setString] = useState("");
   return (
     <SolutionLayout title="Строка">
-     
+      <form
+        className="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <Input maxLength={11} isLimitText />
+        <Button type="submit" extraClass="form__button" text="Развернуть" linkedList="small" />
+        <p>{string}</p>
+      </form>
     </SolutionLayout>
   );
 };
