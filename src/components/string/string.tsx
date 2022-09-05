19a0928349dab43b922f5f1f8ee360b5b54a0c3d
Andrey Arkhipov <arhipov11@yandex.ru>
2022-09-05 18:19:46 +0300

    - Display the sorting of the string - Written drawing and step functions

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,18 +1,61 @@
 import React, {FormEvent, useState} from "react";
+import {DELAY_IN_MS} from "../../constants/delays";
 import {BUTTON_TEXT, MAX_LENGTH_INPUT, TITLE_PAGE} from "../../constants/string-page";
+import {ElementStates} from "../../types/element-states";
 import {Button} from "../ui/button/button";
 import {Circle} from "../ui/circle/circle";
 import {Input} from "../ui/input/input";
 import {SolutionLayout} from "../ui/solution-layout/solution-layout";
 import styles from "./string.module.css";
 
+type TArrForDisplay = [string, ElementStates];
+
 export const StringComponent: React.FC = () => {
   const [string, setString] = useState("");
-  const [isShow, setIsShow] = useState(false);
+  const [arrForDisplay, setArrForDisplay] = useState<TArrForDisplay[]>([]);
+
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    setIsShow(true);
+    draw(string);
   }
+
+  function draw(str: string) {
+    if (string.length === 1) {
+      setArrForDisplay([[str.toUpperCase(), ElementStates.Modified]]);
+      return;
+    }
+
+    let start = 0;
+    let end = string.length - 1;
+
+    const arr: TArrForDisplay[] = Array.from(str).map((el, index) => {
+      if (index === start || index === end) {
+        return [el.toUpperCase(), ElementStates.Changing];
+      }
+      return [el.toUpperCase(), ElementStates.Default];
+    });
+    setArrForDisplay(arr);
+
+    const timerId = setInterval(() => {
+      if (start >= end - 1) clearInterval(timerId);
+      step(arr, start++, end--);
+    }, DELAY_IN_MS);
+  }
+  function step(arr: TArrForDisplay[], start: number, end: number): void {
+    [arr[start], arr[end]] = [arr[end], arr[start]];
+    const newArr: TArrForDisplay[] = arr.map((el, index) => {
+      if (index <= start || index >= end || (index === start + 1 && index === end - 1)) {
+        return [el[0], ElementStates.Modified];
+      }
+      if ((index === start + 1 || index === end - 1) && index !== 0) {
+        return [el[0], ElementStates.Changing];
+      }
+      return el;
+    });
+
+    setArrForDisplay(newArr);
+  }
+
   return (
     <SolutionLayout title={TITLE_PAGE} extraClass={styles.layout}>
       <form className={styles.form} onSubmit={handleSubmit}>
@@ -25,7 +68,9 @@ export const StringComponent: React.FC = () => {
         <Button type="submit" text={BUTTON_TEXT} linkedList="small" />
       </form>
       <section className={styles.visualization}>
-        {isShow && string.split("").map((el, index) => <Circle letter={el} key={index} />)}
+        {arrForDisplay.map((el, index) => (
+          <Circle letter={el[0]} key={index} state={el[1]} />
+        ))}
       </section>
     </SolutionLayout>
   );
