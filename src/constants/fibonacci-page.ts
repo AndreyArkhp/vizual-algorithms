479e20a1e64b77112a5ecc6fd291681969e95da5
Andrey Arkhipov <arhipov11@yandex.ru>
2022-09-08 11:37:09 +0300

    fixed constant

--- src/constants/fibonacci-page.ts
+++ src/constants/fibonacci-page.ts
@@ -1,4 +1,4 @@
 export const MAX__INPUT_NUMBER = 19;
-export const MIN_INPUT_NUMBER = 1;
+export const MIN_INPUT_NUMBER = 0;
 export const TITLE_PAGE = "Последовательность Фибоначчи";
 export const BUTTON_TEXT = "Рассчитать";
