837e7fd188a78ba0c3f288381d3661a0bc829b81
Andrey Arkhipov <arhipov11@yandex.ru>
2022-09-11 14:13:12 +0300

    - Created an initial page structure

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import DocumentTitle from "react-document-title";
+import {MAX_LENGTH_INPUT} from "../../constants/stack-page";
+import {Form} from "../form/form";
+import {Button} from "../ui/button/button";
+import {Input} from "../ui/input/input";
 import {SolutionLayout} from "../ui/solution-layout/solution-layout";
+import styles from "./stack-page.module.css";
 
 export const StackPage: React.FC = () => {
   return (
     <DocumentTitle title="Стек">
-      <SolutionLayout title="Стек"></SolutionLayout>
+      <SolutionLayout title="Стек">
+        <Form handleSubmit={() => {}} extraClass={styles.form}>
+          <fieldset className={styles.form__stackControl}>
+            <Input maxLength={MAX_LENGTH_INPUT} isLimitText />
+            <Button text="Добавить" type="submit" extraClass={styles.form__button} />
+            <Button text="Удалить" type="submit" extraClass={styles.form__button} />
+          </fieldset>
+          <Button text="Очистить" type="submit" extraClass={styles.form__button} />
+        </Form>
+      </SolutionLayout>
     </DocumentTitle>
   );
 };
