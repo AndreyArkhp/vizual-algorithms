dccf56e53d6e1ab69fea76c61cb56b1b07062952
Andrey Arkhipov <arhipov11@yandex.ru>
2022-09-13 14:19:48 +0300

    Created initial structure for list-page

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 import DocumentTitle from "react-document-title";
+import {MAX_LENGTH_INPUT} from "../../constants/list";
+import {Form} from "../form/form";
+import {Button} from "../ui/button/button";
+import {Input} from "../ui/input/input";
 import {SolutionLayout} from "../ui/solution-layout/solution-layout";
+import styles from "./list.module.css";
 
 export const ListPage: React.FC = () => {
   return (
     <DocumentTitle title="Связный список">
-      <SolutionLayout title="Связный список"></SolutionLayout>
+      <SolutionLayout title="Связный список">
+        <Form handleSubmit={() => {}} extraClass={styles.form}>
+          <Input placeholder="Введите значение" maxLength={MAX_LENGTH_INPUT} isLimitText />
+          <Button text="Добавить в head" />
+          <Button text="Добавить в tail" />
+          <Button text="Удалить из head" />
+          <Button text="Удалить из tail" />
+
+          <Input placeholder="Введите индекс" />
+          <Button text="Добавить по индексу" extraClass={styles.form__largeBtn} />
+          <Button text="Удалить по индексу" extraClass={styles.form__largeBtn} />
+        </Form>
+      </SolutionLayout>
     </DocumentTitle>
   );
 };
